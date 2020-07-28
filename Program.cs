becc6cdc0c538a6e677bcb39cb454bbba30eedd0
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-07-28 23:00:21 +0300

    Updated-2.

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace IOCompressionProject
     {
         static void Main(string[] args)
         {
-            string sourceFolder = @"C:\Users\Pavlo\source\repos\IOCompressionProject\bin\Debug\Папка с папкой"; // исходная папка
-            string zipFile = @"C:\Users\Pavlo\source\repos\IOCompressionProject\bin\Debug\Папка для архива\ZIPfile.zip"; // сжатый файл (требует удаления, если уже существует)
-            string targetFolder = @"C:\Users\Pavlo\source\repos\IOCompressionProject\bin\Debug\Папка для рапакованной папки"; // папка, куда распаковывается файл (требует удаления, если уже существует)
+            string sourceFolder = @"C:\Users\Pavlo\source\repos\ConsoleApp-ZipFile\bin\Debug\Папка с папкой"; // исходная папка
+            string zipFile = @"C:\Users\Pavlo\source\repos\ConsoleApp-ZipFile\bin\Debug\Папка для архива\ZIPfile.zip"; // сжатый файл (требует удаления, если уже существует)
+            string targetFolder = @"C:\Users\Pavlo\source\repos\ConsoleApp-ZipFile\bin\Debug\Папка для рапакованной папки"; // папка, куда распаковывается файл (требует удаления, если уже существует)
 
             ZipFile.CreateFromDirectory(sourceFolder, zipFile);
             Console.WriteLine($"Папка «{sourceFolder}» архивирована в файл «{zipFile}»\n\n");
