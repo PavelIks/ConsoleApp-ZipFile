0ce1a64a09da9f39ec3faf22e5085e47b3bdf36f
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-07-28 22:44:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace IOCompressionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFolder = @"C:\Users\Pavlo\source\repos\IOCompressionProject\bin\Debug\Папка с папкой"; // исходная папка
+            string zipFile = @"C:\Users\Pavlo\source\repos\IOCompressionProject\bin\Debug\Папка для архива\ZIPfile.zip"; // сжатый файл (требует удаления, если уже существует)
+            string targetFolder = @"C:\Users\Pavlo\source\repos\IOCompressionProject\bin\Debug\Папка для рапакованной папки"; // папка, куда распаковывается файл (требует удаления, если уже существует)
+
+            ZipFile.CreateFromDirectory(sourceFolder, zipFile);
+            Console.WriteLine($"Папка «{sourceFolder}» архивирована в файл «{zipFile}»\n\n");
+
+            ZipFile.ExtractToDirectory(zipFile, targetFolder);
+            Console.WriteLine($"Файл «{zipFile}» распакован в папку «{targetFolder}»");
+
+            Console.ReadLine();
+        }
+    }
+}
